94749c6a95d367804b2795c25bd07e64f0f0f8ec
Joe Barker <joe.joeb29@gmail.com>
2026-09-15 22:03:47 +0100

    Bump version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.joegec.joycon2android"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
